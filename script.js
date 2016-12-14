2de8e2c0efed433c09e77a82959c64cfed877cba
Peter Kerpedjiev <pkerpedjiev@gmail.com>
2016-12-13 20:37:30 -0500

    Almost working...

--- script.js
+++ script.js
@@ -1,128 +1,309 @@
-var svg = d3.select("svg"),
-    width = +svg.attr("width"),
-    height = +svg.attr("height");
+function selectableForceDirectedGraph() {
+    var width = 600,
 
-var color = d3.scaleOrdinal(d3.schemeCategory20);
+    height = 500,
+    shiftKey, ctrlKey;
 
-var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink()
-            .id(function(d) { return d.id; })
-            .distance(function(d) { 
-                return 30;
-                var dist = 1 / d.value;
-                //console.log('dist:', dist);
-                return dist; 
-            }))
-    .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter(width / 2, height / 2));
+    var nodeGraph = null;
+    var xScale = d3.scaleLinear()
+      .domain([0,width])
+      .range([0,width]);
+    var yScale = d3.scaleLinear()
+     .domain([0,height])
+     .range([0, height]);
 
-function removeGraphcs() {
-    svg.selectAll('g')
-        .remove();
-}
+    var xOrigScale = xScale.copy();
+    var yOrigScale = yScale.copy();
 
-function createGraph(graph) {
-    if (! ("links" in graph)) {
-        console.log("Graph is missing links");
-        return;
+    var svg = d3.select("#d3_selectable_force_directed_graph")
+    .attr("tabindex", 1)
+    .on("keydown.brush", keydown)
+    .on("keyup.brush", keyup)
+    .each(function() { this.focus(); })
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+    var zoomer = d3.zoom().
+        scaleExtent([0.1,10]).
+        on("start", zoomstart).
+        on("zoom", redraw);
+
+    function zoomstart() {
+        node.each(function(d) {
+            d.selected = false;
+            d.previouslySelected = false;
+        });
+        node.classed("selected", false);
     }
 
-    /*
-  var link = svg.append("g")
-      .attr("class", "links")
-    .selectAll("line")
-    .data(graph.links)
-    .enter().append("line")
-      .attr("stroke-width", function(d) { return Math.sqrt(d.value); });
-      */
+    function redraw() {
+        var t = d3.event.transform;
 
-  var node = svg.append("g")
-      .attr("class", "nodes")
-    .selectAll("circle")
-    .data(graph.nodes)
-    .enter().append("circle")
-      .attr("r", 5)
-      .attr("fill", function(d) { 
-          if ('color' in d)
-              return d.color;
-          else
-            return color(d.group); 
-      })
-      .call(d3.drag()
-          .on("start", dragstarted)
-          .on("drag", dragged)
-          .on("end", dragended));
+        xScale.domain(t.rescaleX(xOrigScale).domain());
+        yScale.domain(t.rescaleY(yOrigScale).domain());
 
-  node.append("title")
-      .text(function(d) { 
-          if ('name' in d)
-              return d.name;
-          else
-                return d.id; 
-      });
+        vis.attr("transform", t);
+    }
 
-  simulation
-      .nodes(graph.nodes)
-      .on("tick", ticked);
+    var brusher = d3.brush()
+    //.x(d3.scale.identity().domain([0, width]))
+    //.y(d3.scale.identity().domain([0, height]))
+    //.x(xScale)
+    //.y(yScale)
+    .on("start", function(d) {
+        node.each(function(d) { 
+            d.previouslySelected = shiftKey && d.selected; });
+    })
+    .on("brush", function() {
+        console.log('brush event:', d3.event);
+        var s = d3.event.selection;
+        var sx = s.map(xScale.invert)
 
-  simulation.force("link")
-      .links(graph.links);
+        node.classed("selected", function(d) {
+            return d.selected = d.previouslySelected ^
+            (sx[0][0] <= d.x && d.x < sx[1][0]
+             && sx[0][1] <= d.y && d.y < sx[1][1]);
+        });
+    })
+    .on("end", function() {
+        //d3.event.target.clear();
+        //d3.select(this).call(d3.event.target);
+    });
 
-  function ticked() {
-      /*
-    link
+    var svg_graph = svg.append('svg:g')
+    .call(zoomer)
+    //.call(brusher)
+
+    var rect = svg_graph.append('svg:rect')
+    .attr('width', width)
+    .attr('height', height)
+    .attr('fill', 'transparent')
+    //.attr('opacity', 0.5)
+    .attr('stroke', 'transparent')
+    .attr('stroke-width', 1)
+    //.attr("pointer-events", "all")
+    .attr("id", "zrect")
+
+    var brush = svg_graph.append("g")
+    .datum(function() { return {selected: false, previouslySelected: false}; })
+    .attr("class", "brush");
+
+    var vis = svg_graph.append("svg:g");
+
+    vis.attr('fill', 'red')
+    .attr('stroke', 'black')
+    .attr('stroke-width', 1)
+    .attr('opacity', 0.5)
+    .attr('id', 'vis')
+
+
+    brush.call(brusher)
+    .on("mousedown.brush", null)
+    .on("touchstart.brush", null) 
+    .on("touchmove.brush", null)
+    .on("touchend.brush", null); 
+
+    brush.select('.background').style('cursor', 'auto');
+
+    var link = vis.append("g")
+    .attr("class", "link")
+    .selectAll("line");
+
+    var node = vis.append("g")
+    .attr("class", "node")
+    .selectAll("circle");
+
+    center_view = function() {
+        // Center the view on the molecule(s) and scale it so that everything
+        // fits in the window
+
+        if (nodeGraph === null)
+            return;
+
+        var nodes = nodeGraph.nodes;
+
+        //no molecules, nothing to do
+        if (nodes.length === 0)
+            return;
+
+        // Get the bounding box
+        min_x = d3.min(nodes.map(function(d) {return d.x;}));
+        min_y = d3.min(nodes.map(function(d) {return d.y;}));
+
+        max_x = d3.max(nodes.map(function(d) {return d.x;}));
+        max_y = d3.max(nodes.map(function(d) {return d.y;}));
+
+
+        // The width and the height of the graph
+        mol_width = max_x - min_x;
+        mol_height = max_y - min_y;
+
+        // how much larger the drawing area is than the width and the height
+        width_ratio = width / mol_width;
+        height_ratio = height / mol_height;
+
+        // we need to fit it in both directions, so we scale according to
+        // the direction in which we need to shrink the most
+        min_ratio = Math.min(width_ratio, height_ratio) * 0.8;
+
+        // the new dimensions of the molecule
+        new_mol_width = mol_width * min_ratio;
+        new_mol_height = mol_height * min_ratio;
+
+        // translate so that it's in the center of the window
+        x_trans = -(min_x) * min_ratio + (width - new_mol_width) / 2;
+        y_trans = -(min_y) * min_ratio + (height - new_mol_height) / 2;
+
+
+        // do the actual moving
+        vis.attr("transform",
+                 "translate(" + [x_trans, y_trans] + ")" + " scale(" + min_ratio + ")");
+
+                 // tell the zoomer what we did so that next we zoom, it uses the
+                 // transformation we entered here
+                 console.log('d3.event', d3.event);
+                 svg_graph.call(zoomer.transform, d3.zoomIdentity.scale(min_ratio).translate(x_trans, y_trans));
+
+                 /*
+                 zoomer.translate([x_trans, y_trans ]);
+                 zoomer.scale(min_ratio);
+                 */
+
+    };
+
+
+
+    d3.json("miserables.json", function(error, graph) {
+        nodeGraph = graph;
+
+        link = link.data(graph.links).enter().append("line")
+            /*
         .attr("x1", function(d) { return d.source.x; })
         .attr("y1", function(d) { return d.source.y; })
         .attr("x2", function(d) { return d.target.x; })
         .attr("y2", function(d) { return d.target.y; });
         */
 
-    node
+
+        var simulation = d3.forceSimulation()
+        .force("link", d3.forceLink().id(function(d) { return d.id; }))
+        .force("charge", d3.forceManyBody())
+        .force("center", d3.forceCenter(width / 2, height / 2));
+
+        function dragstarted(d) {
+            d3.event.sourceEvent.stopPropagation();
+            if (!d.selected && !shiftKey) {
+                // if this node isn't selected, then we have to unselect every other node
+                node.classed("selected", function(p) { return p.selected =  p.previouslySelected = false; });
+            }
+
+            d3.select(this).classed("selected", function(p) { d.previouslySelected = d.selected; return d.selected = true; });
+
+            node.filter(function(d) { return d.selected; })
+            .each(function(d) { d.fixed |= 2; })
+
+          if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+          d.fx = d.x;
+          d.fy = d.y;
+        }
+
+        function dragged(d) {
+          d.fx = d3.event.x;
+          d.fy = d3.event.y;
+        }
+
+        function dragended(d) {
+          if (!d3.event.active) simulation.alphaTarget(0);
+          d.fx = null;
+          d.fy = null;
+        }
+
+        node = node.data(graph.nodes).enter().append("circle")
+        .attr("r", 4)
         .attr("cx", function(d) { return d.x; })
-        .attr("cy", function(d) { return d.y; });
-  }
-};
+        .attr("cy", function(d) { return d.y; })
+        .on("dblclick", function(d) { d3.event.stopPropagation(); })
+        .on("click", function(d) {
+            if (d3.event.defaultPrevented) return;
 
-function dragstarted(d) {
-  if (!d3.event.active) simulation.alphaTarget(0.9).restart();
-  d.fx = d.x;
-  d.fy = d.y;
+            if (!shiftKey) {
+                //if the shift key isn't down, unselect everything
+                node.classed("selected", function(p) { return p.selected =  p.previouslySelected = false; })
+            }
+
+            // always select this node
+            d3.select(this).classed("selected", d.selected = !d.previouslySelected);
+        })
+
+        .on("mouseup", function(d) {
+            //if (d.selected && shiftKey) d3.select(this).classed("selected", d.selected = false);
+        })
+        .call(d3.drag()
+              .on("start", dragstarted)
+              .on("drag", dragged)
+              .on("end", dragended));
+
+              function ticked() {
+                  link.attr("x1", function(d) { return d.source.x; })
+                  .attr("y1", function(d) { return d.source.y; })
+                  .attr("x2", function(d) { return d.target.x; })
+                  .attr("y2", function(d) { return d.target.y; });
+
+                  node.attr('cx', function(d) { return d.x; })
+                  .attr('cy', function(d) { return d.y; });
+
+              };
+
+            simulation
+                      .nodes(graph.nodes)
+                            .on("tick", ticked);
+
+            console.log('graph', graph);
+              simulation.force("link")
+                        .links(graph.links);
+
+    });
+
+
+    function keydown() {
+        shiftKey = d3.event.shiftKey || d3.event.metaKey;
+        ctrlKey = d3.event.ctrlKey;
+
+        console.log('d3.event', d3.event)
+
+        if (d3.event.keyCode == 67) {   //the 'c' key
+            center_view();
+        }
+
+        if (shiftKey) {
+            svg_graph.call(zoomer)
+            .on("mousedown.zoom", null)
+            .on("touchstart.zoom", null)                                                                      
+            .on("touchmove.zoom", null)                                                                       
+            .on("touchend.zoom", null);                                                                       
+
+            //svg_graph.on('zoom', null);                                                                     
+            vis.selectAll('g.gnode')
+            .on('mousedown.drag', null);
+
+            brush.select('.background').style('cursor', 'crosshair')
+            brush.call(brusher);
+        }
+    }
+
+    function keyup() {
+        shiftKey = d3.event.shiftKey || d3.event.metaKey;
+        ctrlKey = d3.event.ctrlKey;
+
+        brush.call(brusher)
+        .on("mousedown.brush", null)
+        .on("touchstart.brush", null)                                                                      
+        .on("touchmove.brush", null)                                                                       
+        .on("touchend.brush", null);                                                                       
+
+        brush.select('.background').style('cursor', 'auto')
+        svg_graph.call(zoomer);
+    }
 }
-
-function dragged(d) {
-  d.fx = d3.event.x;
-  d.fy = d3.event.y;
-}
-
-function dragended(d) {
-  if (!d3.event.active) simulation.alphaTarget(0);
-  d.fx = null;
-  d.fy = null;
-}
-
-// load dataset and create table
-function load_dataset(jsonText) {
-  var data = JSON.parse(jsonText)
-  console.log('data:', data);
-  createGraph(data);
-  simulation.alphaTarget(0.3).restart();
-}
-
-// handle upload button
-function upload_button(el, callback) {
-  var uploader = document.getElementById(el);  
-  var reader = new FileReader();
-
-  reader.onload = function(e) {
-    var contents = e.target.result;
-    callback(contents);
-  };
-
-  uploader.addEventListener("change", handleFiles, false);  
-
-  function handleFiles() {
-    var file = this.files[0];
-    reader.readAsText(file);
-  };
-};
 
